57b69f1d24bd20c8c891711ee7081cc396789c29
itsarthurmorgan <140511073+itsarthurmorgan@users.noreply.github.com>
2023-08-16 08:46:45 +0530

    Update mean-mode-median.r

--- mean-mode-median.r
+++ mean-mode-median.r
@@ -1,15 +1,19 @@
 x <- c(1, 23, 56, 23, 89, 4, 2, 9)
+
 result.mean <- mean(x)
-median.result = median(x)
-print ("median: ", median.result)
+median.result <- median(x)
 result.variance <- var(x)
 result.sd <- sqrt(var(x))
-print("mean: ", result.mean)
-print("variance: ", result.variance)
-print("sd: ", result.sd)
+
+cat("median: ", median.result, "\n")
+cat("mean: ", result.mean, "\n")
+cat("variance: ", result.variance, "\n")
+cat("sd: ", result.sd, "\n")
+
 mode <- function(x) {
- ux <- unique(x)
- ux[which.max(tabulate(match(x, ux)))]
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
 }
-mode.result = mode(x)
-print ("mode: ", mode.result)
+
+mode.result <- mode(x)
+cat("mode: ", mode.result, "\n")
