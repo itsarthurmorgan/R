f3383a277fdb63dcf98603b682ac49aa57730b72
itsarthurmorgan <140511073+itsarthurmorgan@users.noreply.github.com>
2023-08-16 07:57:35 +0530

    Update class-modify-methods.r

--- class-modify-methods.r
+++ class-modify-methods.r
@@ -1,5 +1,5 @@
-setRefClass("students", fields = list(name = "character", age = "numeric", gpa = "numeric"))
-students <- setRefClass("students", fields = list(name = "character", age = "numeric", gpa =
-"numeric"))
-s <- students( name = "arthur", age = 21, gpa = 3.5)
+Student <- setRefClass("Student", 
+  fields = list(name = "character", age = "numeric", gpa = "numeric"))
+
+s <- Student$new(name = "arthur", age = 21, gpa = 3.5)
 s
